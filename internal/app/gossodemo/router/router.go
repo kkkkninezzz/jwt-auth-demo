b5b3e242726a84498921adb3ed21515eca231b8e
kurisu <3099355091@qq.com>
2021-06-22 23:38:59 +0800

    搭建基础的router

--- internal/app/gossodemo/router/router.go
+++ internal/app/gossodemo/router/router.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	"gossodemo/internal/app/gossodemo/handler"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupRoutes(app *fiber.App) {
-
+	api := app.Group("/api", logger.New())
+	api.Get("/", handler.Hello)
 }
