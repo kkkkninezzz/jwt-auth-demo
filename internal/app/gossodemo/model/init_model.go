03fbe7c43809e7938ee7cfd272d52233ad03a6e0
kurisu <3099355091@qq.com>
2021-06-23 23:44:49 +0800

    调整model的类型以及表名规则

--- internal/app/gossodemo/model/init_model.go
+++ internal/app/gossodemo/model/init_model.go
@@ -3,6 +3,7 @@ package model
 import "gossodemo/internal/pkg/database"
 
 func Init() {
-	database.DB.AutoMigrate(&Product{})
-	database.DB.AutoMigrate(&UserBase{})
+	db := database.DB
+	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&UserBase{})
 }
--- internal/pkg/database/connect.go
+++ internal/pkg/database/connect.go
@@ -5,12 +5,18 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var DB *gorm.DB
 
 func Connect(dsn string) {
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "t_", // table name prefix, table for `User` would be `t_users`
+			SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
+		},
+	})
 	if err != nil {
 		panic(err)
 	}
