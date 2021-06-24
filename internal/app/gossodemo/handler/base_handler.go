f1b65212c49a821538d71dcfcd3004761980b5c5
18581489108 <18581489108@163.com>
2021-06-24 16:11:51 +0800

    增加handler统一的error返回

--- internal/app/gossodemo/handler/base_handler.go
+++ internal/app/gossodemo/handler/base_handler.go
@@ -52,12 +52,12 @@ func validateStruct(data interface{}) []*ErrorResponse {
 // 从body中反序列化并且进行验证
 func bodyParserAndValidate(out interface{}, ctx *fiber.Ctx) error {
 	if err := ctx.BodyParser(out); err != nil {
-		return ctx.SendStatus(fiber.StatusBadRequest)
+		return BadRequestError(ctx, err)
 	}
 
 	errors := validateStruct(out)
 	if errors != nil {
-		return ctx.JSON(errors)
+		return BadRequestError(ctx, errors)
 	}
 
 	return nil
@@ -65,4 +65,36 @@ func bodyParserAndValidate(out interface{}, ctx *fiber.Ctx) error {
 
 func InitValidator() {
 	validate = validator.New()
+}
+
+func Error(ctx *fiber.Ctx, status int, msg string, data interface{}) error {
+	return ctx.Status(status).JSON(fiber.Map{
+		"status":  "error",
+		"message": msg,
+		"data":    data,
+	})
+}
+
+// 未验证的错误信息
+func UnauthorizedError(ctx *fiber.Ctx, msg string, data interface{}) error {
+	return Error(ctx, fiber.StatusUnauthorized, msg, data)
+}
+
+// 参数错误error信息
+func BadRequestError(ctx *fiber.Ctx, data interface{}) error {
+	return Error(ctx, fiber.StatusBadRequest, "Param is invaild", data)
+}
+
+// 服务器异常error信息
+func InternalServerError(ctx *fiber.Ctx, msg string, data interface{}) error {
+	return Error(ctx, fiber.StatusInternalServerError, msg, data)
+}
+
+// 成功的消息
+func SuccessError(ctx *fiber.Ctx, msg string, data interface{}) error {
+	return ctx.JSON(fiber.Map{
+		"status":  "success",
+		"message": msg,
+		"data":    data,
+	})
 }
